a4ec70a4871c50f358050a6ceca9b6865c60d872
Jake <jacobvictorberg@gmail.com>
2018-07-18 13:35:43 -0700

    changed css name from videoOverlay to header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Container } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 // HEY! Why don't dis work??? 
 // import '../semantic/dist/semantic.min.css';
-import './css/videoOverlay.css';
+import './css/header.css';
 import  Header  from './Components/Header';
 import  Section  from './Components/Section';
 
